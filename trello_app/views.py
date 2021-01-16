98df2e468bc4db96d5ff43e209adf990ad98a1fa
Naoki Imai <naokinetin0110@gmail.com>
2021-01-16 10:56:07 +0900

    clean_html_form

--- trello_app/views.py
+++ trello_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect, resolve_url
+from django.shortcuts import render,redirect, resolve_url, get_object_or_404
 from django.urls import reverse_lazy
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
@@ -7,7 +7,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.views.generic import DetailView, UpdateView, CreateView, ListView, DeleteView
 
-from .forms import UserForm, ListForm, CardForm
+from .forms import UserForm, ListForm, CardForm, CardCreateFromHomeForm
 from .models import List, Card
 from .mixins import OnlyYouMixin
 
@@ -17,9 +17,9 @@ def index(request):
   return render(request, "trello_app/index.html")
 
 #ログインしたらhomeへリダイレクトする
-@login_required
-def home(request):
-  return render(request, "trello_app/home.html")
+class HomeView(LoginRequiredMixin, ListView):
+  model = List
+  template_name = 'trello_app/home.html'
 
 def signup(request):
   if request.method == 'POST':
@@ -52,7 +52,7 @@ class ListCreateView(LoginRequiredMixin, CreateView):
   model = List
   template_name = 'trello_app/lists/create.html'
   form_class = ListForm
-  success_url = reverse_lazy('trello_app:lists_list')
+  success_url = reverse_lazy('trello_app:home')
 
   def form_valid(self, form):
     form.instance.user = self.request.user
@@ -70,21 +70,19 @@ class ListUpdateView(LoginRequiredMixin,UpdateView):
   model = List
   template_name = "trello_app/lists/update.html"
   form_class = ListForm
-# resolve_url 正常ならusers_detaiに接続する.pkはDBのpk
-  def get_success_url(self):
-    return resolve_url('trello_app:lists_detail', pk=self.kwargs['pk'])
+  success_url = reverse_lazy('trello_app:home')
 
 class ListDeleteView(LoginRequiredMixin, DeleteView):
   model = List
   template_name = 'trello_app/lists/delete.html'
   form_class = ListForm
-  success_url = reverse_lazy('trello_app:lists_list')
+  success_url = reverse_lazy('trello_app:home')
 
 class CardCreateView(LoginRequiredMixin, CreateView):
   model = Card
   template_name = 'trello_app/cards/create.html'
   form_class = CardForm
-  success_url = reverse_lazy('trello_app:cards_list')
+  success_url = reverse_lazy('trello_app:home')
 
   def form_valid(self, form):
     form.instance.user = self.request.user
@@ -105,10 +103,23 @@ class CardUpdateView(LoginRequiredMixin,UpdateView):
   form_class = CardForm
 # resolve_url 正常ならusers_detaiに接続する.pkはDBのpk
   def get_success_url(self):
-    return resolve_url('trello_app:cards_list')
+    return resolve_url('trello_app:home')
 
 class CardDeleteView(LoginRequiredMixin, DeleteView):
   model = Card
   template_name = 'trello_app/cards/delete.html'
   form_class = CardForm
-  success_url = reverse_lazy('trello_app:cards_list')
+  success_url = reverse_lazy('trello_app:home')
+
+class CardCreateFromHomeView(LoginRequiredMixin, CreateView):
+  model = Card
+  template_name = 'trello_app/cards/create.html'
+  form_class = CardCreateFromHomeForm
+  success_url = reverse_lazy('trello_app:home')
+
+  def form_valid(self, form):
+    list_pk = self.kwargs['list_pk']
+    list_instance = get_object_or_404(List, pk=list_pk)
+    form.instance.list = list_instance
+    form.instance.user = self.request.user
+    return super().form_valid(form)
