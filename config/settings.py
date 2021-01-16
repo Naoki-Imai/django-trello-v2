e5f4d0d3f9c95537a26d68c27bb91543ce177c4b
Naoki Imai <naokinetin0110@gmail.com>
2021-01-16 14:58:50 +0900

    finish

--- config/settings.py
+++ config/settings.py
@@ -81,8 +81,8 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'password',
-        'HOST': 'db',
-        'PORT': 5432,
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
